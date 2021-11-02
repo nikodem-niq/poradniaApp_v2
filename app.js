9da4308205b9444398e9eab2b62320a506ebec2e
Nikodem Zychma <nikodemniq@Nikodems-MacBook-Pro.local>
2021-11-03 00:42:12 +0100

    fix: path and serving react files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+// app.use(express.static());
 
 app.use((err, req, res, next) => {
     res.locals.error = err;
@@ -26,8 +27,12 @@ app.use('/updateData', updateDataRoutes)
 const userRoutes = require('./routes/userRoutes');
 app.use('/user', userRoutes)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 
 module.exports = app;
