da908436d4dfcf523a22a4a5e9199f2196219126
Nikodem Zychma <nikodemniq@Nikodems-MacBook-Pro.local>
2021-11-03 00:04:17 +0100

    fix: add path

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
